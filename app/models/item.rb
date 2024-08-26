a4d1084cb3d680a614528b8c4cc62e27fef5b5f9
emma2033 <ysndi.7382@gmail.com>
2024-08-26 14:18:31 +0900

    バリデーションの設定

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :image
+    validates :category_id, numericality: { other_than: 1 }
+    validates :condition_id, numericality: { other_than: 1 }
+    validates :shipping_fee_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :shipping_day_id, numericality: { other_than: 1 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+end
