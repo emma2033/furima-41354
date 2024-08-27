b560f457f43f3131a8b2bed0f3b8dd3c0b5a441a
emma2033 <ysndi.7382@gmail.com>
2024-08-27 22:03:44 +0900

    ビューファイルの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,4 +15,11 @@ class ItemsController < ApplicationController
       render :new
     end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
 end
