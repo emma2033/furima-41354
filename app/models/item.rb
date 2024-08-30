6644d0a67b4375619c4a8b0f870d24ae6d003521
emma2033 <ysndi.7382@gmail.com>
2024-08-30 21:32:18 +0900

    エラーメッセージ表示の修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,22 +1,23 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
-  belongs_to :category
-  belongs_to :condition
-  belongs_to :shipping_fee
-  belongs_to :prefecture
-  belongs_to :shipping_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_day
+
   has_one_attached :image
 
   with_options presence: true do
     validates :name
     validates :description
-    validates :image
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :shipping_fee_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :shipping_day_id, numericality: { other_than: 1 }
+    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :image
   end
 end
