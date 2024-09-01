33801bbe2d62e945ef48bdc70eea53044c78cdb7
emma2033 <ysndi.7382@gmail.com>
2024-09-02 02:13:54 +0900

    単体テストコードの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,12 +12,15 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options presence: true, numericality: { other_than: 1, message: 'must be other than 1' } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_fee_id
+    validates :prefecture_id
+    validates :shipping_day_id
   end
 end
