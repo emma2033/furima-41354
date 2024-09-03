266d841650a9ef51ba44c1a7dfcceaf9ab73aa59
emma2033 <ysndi.7382@gmail.com>
2024-09-03 16:14:56 +0900

    商品詳細表示ページへ遷移の設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:show]
   def index
     @items = Item.includes(:user).order(created_at: :desc)
   end
@@ -17,11 +18,14 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def show
-  # @item = Item.find(params[:id])
-  # end
+  def show
+  end
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
